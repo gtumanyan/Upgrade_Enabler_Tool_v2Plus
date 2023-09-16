24886e2caaf6d80138c10881d36e27cc6b3f8d12
WindR <gtumanyan@mail.ru>
2023-09-16 21:25:18 +0300

    Add a script for direct install.esd modification on the x: drive

---
+++ ModifyESD.cmd
@@ -0,0 +1,46 @@
+@setlocal DisableDelayedExpansion
+@echo off
+REM change wording if needed for echo commands..
+SET "Version=v1.1+"
+
+
+:: Re-launch the script with x64 process if it was initiated by x86 process on x64 bit Windows
+
+if exist %SystemRoot%\Sysnative\cmd.exe (
+set "_cmdf=%~f0"
+setlocal EnableDelayedExpansion
+start %SystemRoot%\Sysnative\cmd.exe /c ""!_cmdf!" %*"
+exit /b
+)
+
+TITLE Modify Windows install.esd To Be Able To Directly Upgrade From any Windows Install %version%
+
+echo===============================================
+echo Check for Admin rights...
+echo===============================================
+if not "%1"=="am_admin" powershell start -verb runas '%0' am_admin & exit /b
+
+:gotAdmin
+pushd "%CD%"
+CD /D "%~dp0"
+cls
+
+set "_wimlib=%~dp0bin\wimlib-imagex.exe"
+
+for /f "tokens=2 delims=: " %%# in ('dism.exe /english /get-wiminfo /wimfile:"x:\sources\install.esd" /index:1 ^| find /i "Architecture"') do set warch=%%#
+echo.
+echo===============================================
+echo Adding the modified %warch% UpgradeMatrix.xml to the %warch% install.esd...
+echo===============================================
+echo.
+for /f "tokens=3 delims=: " %%i in ('%_wimlib% info x:\sources\install.esd ^| findstr /c:"Image Count"') do set images=%%i
+for /L %%i in (1,1,%images%) do (
+  %_wimlib% update "x:\Sources\install.esd" %%i --command="add 'UpgradeMatrix\%warch%\UpgradeMatrix.xml' '\Windows\servicing\Editions\UpgradeMatrix.xml'"
+)
+echo.
+echo===============================================
+echo Optimizing the ESD file...
+echo===============================================
+echo.
+%_wimlib% optimize "x:\Sources\install.esd"
+echo.
