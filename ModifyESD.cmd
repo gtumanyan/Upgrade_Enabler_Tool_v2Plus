df0a77b143d640dddbbbf5470350d61a36d2f16f
WindR <gtumanyan@mail.ru>
2024-05-09 18:22:49 +0300

    Get admin rights with nircmd

--- ModifyESD.cmd
+++ ModifyESD.cmd
@@ -10,8 +10,13 @@ REM When set to "0" = No install.wim optimizing
 REM When set to "1" = Install.wim will be optimized, this can use all system resources and take a while
 SET "Optimize=0"
 REM When set to "0" the EI.CFG file won't be added
-REM When set to "1" the EI.CFG file will be added (when there already is an EI.CFG it will be ranemed to EI.CFG.ORI)
+REM When set to "1" the EI.CFG file will be added (when there already is an EI.CFG it will be renamed to EI.CFG.ORI)
 SET "EI_CFG_ADD=1"
+
+echo===============================================
+echo Check for Admin rights and elevate...
+echo===============================================
+	reg query "HKU\S-1-5-19" >nul 2>&1 || %NH% elevate "%~f0" && exit
 
 :: Re-launch the script with x64 process if it was initiated by x86 process on x64 bit Windows
 
@@ -37,15 +42,6 @@ exit /b
 
 TITLE Modify Windows %WIMFILE% To Be Able To Directly Upgrade From any Windows Install %version%
 
-echo===============================================
-echo Check for Admin rights...
-echo===============================================
-if not "%1"=="am_admin" powershell start -verb runas '%0' am_admin & exit /b
-
-:gotAdmin
-pushd "%CD%"
-CD /D "%~dp0"
-cls
 
 set "_wimlib=%~dp0bin\wimlib-imagex.exe"
 
@@ -67,12 +63,13 @@ Goto :Cleanup
 )
 
 SET "Winver="
-IF %vermajor% EQU 22000 SET "Winver=22000" & GOTO :SetISOFiX
-IF %vermajor% EQU 22621 SET "Winver=22621" & GOTO :SetISOFiX
+IF %vermajor% EQU 22000 SET "Winver=22000" & GOTO :SkipSetISOFiX
+IF %vermajor% EQU 22621 SET "Winver=22621" & GOTO :SkipSetISOFiX
+IF %vermajor% EQU 26100 SET "Winver=26100" & GOTO :SkipSetISOFiX
 
-IF %vermajor% EQU 9600 SET "Winver=9600" & SET "ISOFiX=0" & GOTO :SKiPSetISOFiX
-IF %vermajor% EQU 17763 SET "Winver=17763" & SET "ISOFiX=0" & GOTO :SKiPSetISOFiX
-IF %vermajor% EQU 19041 SET "Winver=1904x" & SET "ISOFiX=0" & GOTO :SKiPSetISOFiX
+IF %vermajor% EQU 9600 SET "Winver=9600" & SET "ISOFiX=0" & GOTO :SkipSetISOFiX
+IF %vermajor% EQU 17763 SET "Winver=17763" & SET "ISOFiX=0" & GOTO :SkipSetISOFiX
+IF %vermajor% EQU 19041 SET "Winver=1904x" & SET "ISOFiX=0" & GOTO :SkipSetISOFiX
 
 IF NOT DEFINED Winver (
 ::if exist "TEMP" rmdir /q /s "TEMP"
@@ -84,7 +81,7 @@ echo.
 Goto :Cleanup
 )
 
-:SKiPSetISOFiX
+:SkipSetISOFiX
 
 for /f "tokens=2 delims=: " %%# in ('dism.exe /english /get-wiminfo /wimfile:"x:\sources\%WIMFILE%" /index:1 ^| find /i "Architecture"') do set warch=%%#
 echo.
