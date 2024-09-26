d5b71f1bf21c7e01e4c89a2ea3b24543b01d8626
WindR <gtumanyan@mail.ru>
2024-09-26 07:50:16 +0300

    Update wimlib

--- Bin/_update.bat
+++ Bin/_update.bat
@@ -9,7 +9,8 @@ set "pp=%~dp0"
 set "pe=%~dp0%exe%" 
 call "%~dp0..\..\toolset.bat" "%~1" || goto SKIP
 ::BEGIN
-call %spider% || goto SKIP
+call %spider% || (call %msg% 11 & goto SKIP)
+call %iflast% && goto SKIP
 call %close% "%pe%" || goto SKIP
 call %download_distr% || (call %msg% 3 & goto SKIP)
 %unpack_distr% -x!devel -x!COPYING* || (call %msg% 4 & goto SKIP)
